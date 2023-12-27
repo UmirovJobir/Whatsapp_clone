0f224832536118e39a0d28482a9b9a977cf41952
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-12-27 14:16:45 +0500

    docker redis

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -91,14 +91,25 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": env.str("SQL_ENGINE", "django.db.backends.sqlite3"),
+#         "NAME": env.str("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+#         "USER": env.str("SQL_USER", "user"),
+#         "PASSWORD": env.str("SQL_PASSWORD", "password"),
+#         "HOST": env.str("SQL_HOST", "localhost"),
+#         "PORT": env.str("SQL_PORT", "5432"),
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": env.str("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": env.str("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": env.str("SQL_USER", "user"),
-        "PASSWORD": env.str("SQL_PASSWORD", "password"),
-        "HOST": env.str("SQL_HOST", "localhost"),
-        "PORT": env.str("SQL_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat_db',
+        'USER': 'jobir',
+        'PASSWORD': '5Noyabr2022',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
